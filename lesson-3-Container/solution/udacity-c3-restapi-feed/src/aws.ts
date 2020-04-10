e7c6b82b200127f72c266c7b1eb04cafebb703ba
Ulhas Pai <upai@kayak.com>
2020-04-10 18:20:25 -0400

    Lesson 3 changes

--- lesson-3-Container/solution/udacity-c3-restapi-feed/src/aws.ts
+++ lesson-3-Container/solution/udacity-c3-restapi-feed/src/aws.ts
@@ -3,13 +3,13 @@ import { config } from './config/config';
 
 const c = config.dev;
 
-//Configure AWS
+// Configure AWS
 var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: c.aws_reigion,
+  // region: c.aws_reigion,
   params: {Bucket: c.aws_media_bucket}
 });
 
